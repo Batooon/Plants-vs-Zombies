b2d3fadab46600c303ecaea2b2ff58ea1f74b7b6
Anton <rozum.ant@gmail.com>
2021-01-12 21:48:45 +0200

    PlantShop.cs created

---
+++ Assets/Scripts/UI/PlantsShop.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Data;
+using UnityEngine;
+
+namespace UI
+{
+    public class PlantsShop : MonoBehaviour
+    {
+        [SerializeField] private GameObject _plantCardPrefab;
+        [SerializeField] private List<PlantData> _plantDatas;
+        [SerializeField] private GameObject _shopParent;
+
+        public void Init()
+        {
+            if (_plantCardPrefab.TryGetComponent(out PlantCardPresenter plantCardPresenter))
+            {
+                foreach (var plantData in _plantDatas)
+                {
+                    GameObject plantCard = Instantiate(_plantCardPrefab, _shopParent.transform);
+                    if (plantCard.TryGetComponent(out PlantCardPresenter plant))
+                    {
+                        plant.Init(plantData);
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("Plant card prefab does not contain a PlantCardPresenter.cs");
+            }
+        }
+    }
+}
