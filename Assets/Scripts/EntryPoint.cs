8721a885f252abb22bdc89105bc35302cceddc8d
Anton <rozum.ant@gmail.com>
2021-01-12 21:48:10 +0200

    EntryPoint.cs created

---
+++ Assets/Scripts/EntryPoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UI;
+using UnityEngine;
+
+public class EntryPoint : MonoBehaviour
+{
+    [SerializeField] private PlantsShop _plantsShop;
+    
+    private void Awake()
+    {
+        _plantsShop.Init();
+    }
+}
