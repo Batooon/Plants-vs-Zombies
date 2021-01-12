88c25c536f3a329e857148d1de983bffe036e46f
Anton <rozum.ant@gmail.com>
2021-01-12 23:50:14 +0200

    encapsulated data

--- Assets/Scripts/Data/PlantData.cs
+++ Assets/Scripts/Data/PlantData.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Data
 {
     [Serializable]
     public class PlantData
     {
-        public Sprite _icon;
-        public int _cost;
+        [SerializeField] private Sprite _icon;
+        [SerializeField] private int _cost;
+        [SerializeField] private GameObject _templatePlant;
+
+        public Sprite Icon => _icon;
+        public int Cost => _cost;
+        public GameObject TemplatePlant => _templatePlant;
     }
 }
