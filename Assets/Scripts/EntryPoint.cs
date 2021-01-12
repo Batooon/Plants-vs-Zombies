de8065b8197a1bd20034392bcbd23089d84ec069
Anton <rozum.ant@gmail.com>
2021-01-12 23:48:45 +0200

    added plant data list and shop now initialize with that data

--- Assets/Scripts/EntryPoint.cs
+++ Assets/Scripts/EntryPoint.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Data;
 using UI;
 using UnityEngine;
 
 public class EntryPoint : MonoBehaviour
 {
     [SerializeField] private PlantsShop _plantsShop;
+    [SerializeField] private List<PlantData> _plantDatas;
     
     private void Awake()
     {
-        _plantsShop.Init();
+        _plantsShop.Init(_plantDatas);
     }
 }
