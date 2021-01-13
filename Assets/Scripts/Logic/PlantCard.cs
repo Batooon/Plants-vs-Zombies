cb5379b80d369eeea156a748fd7e797a7c4c3f64
Anton <rozum.ant@gmail.com>
2021-01-13 03:16:44 +0200

    PlantCard.cs changed

--- Assets/Scripts/Logic/PlantCard.cs
+++ Assets/Scripts/Logic/PlantCard.cs
@@ -3,34 +3,54 @@ using Data;
 using UI;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 namespace Logic
 {
     [RequireComponent(typeof(PlantCardPresenter))]
-    public class PlantCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    public class PlantCard : MonoBehaviour
     {
-        private GameObject _templatePlant;
-        
-        public void Init(PlantData plantData)
-        {
-            _templatePlant = plantData.TemplatePlant;
-            GetComponent<PlantCardPresenter>().Init(plantData);
-        }
-        
-        public void OnBeginDrag(PointerEventData eventData)
-        {
-            //Instantiate plant prefab
-            GameObject tempPlant = Instantiate(_templatePlant, eventData.position, Quaternion.identity);
+        private PlantTemplate _templatePlant;
+        private bool _isCardAvailable = true;
+        private Tilemap _tilemap;
 
+        public void Init(PlantShopData plantShopData, Tilemap tilemap)
+        {
+            _tilemap = tilemap;
+            _templatePlant = plantShopData.TemplatePlant;
+            GetComponent<PlantCardPresenter>().Init(plantShopData);
+        }
+
+        private void Start()
+        {
+            SpawnTemplate();
+        }
+
+        private void SpawnTemplate()
+        {
+            RectTransform rectTransform = GetComponent<RectTransform>();
+            Vector2 spawnPosition = rectTransform.position;
+            PlantTemplate plantTemplate = Instantiate(_templatePlant.gameObject, spawnPosition, Quaternion.identity)
+                .GetComponent<PlantTemplate>();
+            plantTemplate.Init(spawnPosition, _tilemap);
+        }
+
+        /*private void OnMouseDrag()
+        {
+            if (_isCardAvailable == false)
+                return;
+            _isCardAvailable = false;
+
+            Vector2 spawnPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GameObject tempPlant = Instantiate(_templatePlant.gameObject, spawnPosition, Quaternion.identity);
+            
             //TODO: Сделать затемнение карточки, и постепенное её восстановление
-        }
+        }*/
 
-        public void OnDrag(PointerEventData eventData)
+        private void OnCardRestored()
         {
-        }
-
-        public void OnEndDrag(PointerEventData eventData)
-        {
+            _isCardAvailable = true;
         }
     }
 }
