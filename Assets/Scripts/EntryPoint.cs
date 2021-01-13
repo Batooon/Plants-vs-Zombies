e71c86f8cd8b832bdfd8f7b9fceebef31a712040
Anton <rozum.ant@gmail.com>
2021-01-13 03:18:46 +0200

    EntryPoint.cs changed

--- Assets/Scripts/EntryPoint.cs
+++ Assets/Scripts/EntryPoint.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Data;
 using UI;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class EntryPoint : MonoBehaviour
 {
     [SerializeField] private PlantsShop _plantsShop;
-    [SerializeField] private List<PlantData> _plantDatas;
+    [SerializeField] private List<PlantShopData> _plantDatas;
+    [SerializeField] private Tilemap _tilemap;
     
     private void Awake()
     {
-        _plantsShop.Init(_plantDatas);
+        _plantsShop.Init(_plantDatas, _tilemap);
     }
 }
