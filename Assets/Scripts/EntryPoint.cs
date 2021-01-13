4395d6c4649839994285baa7b876049592ad1104
Anton <rozum.ant@gmail.com>
2021-01-14 00:10:28 +0200

    Initializing field

--- Assets/Scripts/EntryPoint.cs
+++ Assets/Scripts/EntryPoint.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Data;
 using UI;
 using UnityEngine;
@@ -11,9 +9,16 @@ public class EntryPoint : MonoBehaviour
     [SerializeField] private PlantsShop _plantsShop;
     [SerializeField] private List<PlantShopData> _plantDatas;
     [SerializeField] private Tilemap _tilemap;
+    [SerializeField] private int _fieldWidth;
+    [SerializeField] private int _fieldHeight;
+    [SerializeField] private GameObject _plant;
+
+    private Field _plantsField;
     
     private void Awake()
     {
         _plantsShop.Init(_plantDatas, _tilemap);
+        
+        _plantsField = new Field(_fieldWidth, _fieldHeight, _tilemap);
     }
 }
