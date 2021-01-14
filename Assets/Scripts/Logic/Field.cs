8321c0c5fa9d74dbd71d22c12d83bc5d5633d9cf
Anton <rozum.ant@gmail.com>
2021-01-15 01:57:36 +0200

    Created some useful methods and place plant method changed
    Check if the pointer is over field, get cell center coordinates

--- Assets/Scripts/Logic/Field.cs
+++ Assets/Scripts/Logic/Field.cs
@@ -1,47 +1,75 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class Field
+namespace Logic
 {
-    private Cell[,] _cells;
-    private int _width;
-    private int _height;
-    private Tilemap _renderField;
+    public class Field
+    {
+        private Cell[,] _cells;
+        private readonly int _width;
+        private readonly int _height;
+        private readonly Tilemap _renderField;
     
-    public Field(int width, int height, Tilemap renderField)
-    {
-        _width = width;
-        _height = height;
-        _renderField = renderField;
-        GenerateEmptyField();
-    }
-
-    public void PlacePlant(Vector3 mousePosition, GameObject plant)
-    {
-        Vector3Int cellPosition = GetCellCoordinates(mousePosition);
-
-        if (_cells[cellPosition.x, cellPosition.y].IsEmpty == false)
-            return;
-
-        GameObject instantiatedPlant = Object.Instantiate(plant, cellPosition, Quaternion.identity);
-    }
-
-    private Vector3Int GetCellCoordinates(Vector3 mousePosition)
-    {
-        Vector3 worldPoint = Camera.main.ScreenToWorldPoint(mousePosition);
-
-        Vector3Int cellPosition = _renderField.WorldToCell(worldPoint);
-        return cellPosition;
-    }
-    
-    private void GenerateEmptyField()
-    {
-        _cells = new Cell[_width, _height];
-        for (int i = 0; i < _cells.GetLength(0); i++)
+        public Field(int width, int height, Tilemap renderField)
         {
-            for (int j = 0; j < _cells.GetLength(1); j++)
+            _width = width;
+            _height = height;
+            _renderField = renderField;
+            GenerateEmptyField();
+        }
+
+        public void TryPlacePlant(Vector3 mousePosition, GameObject plant)
+        {
+            var cellPosition = GetCellCoordinates(mousePosition);
+
+            if (IsOnFieldCriteria(cellPosition) == false)
+                return;
+            
+            if (_cells[cellPosition.x, cellPosition.y].IsEmpty == false)
+                return;
+
+            Object.Instantiate(plant, GetCellCenterCoordinates(mousePosition), Quaternion.identity);
+            _cells[cellPosition.x, cellPosition.y].IsEmpty = false;
+        }
+
+        public bool IsPointerOverField(Vector3 pointerPosition)
+        {
+            var cellPosition = GetCellCoordinates(pointerPosition);
+
+            return IsOnFieldCriteria(cellPosition);
+        }
+
+        private bool IsOnFieldCriteria(Vector3Int cell)
+        {
+            return _cells.GetLength(0) > cell.x
+                   && _cells.GetLength(1) > cell.y
+                   && cell.x >= 0
+                   && cell.y >= 0;
+        }
+
+        public Vector3 GetCellCenterCoordinates(Vector3 mousePosition)
+        {
+            var cellPosition = GetCellCoordinates(mousePosition);
+            
+            return _renderField.GetCellCenterWorld(cellPosition);
+        }
+
+        private Vector3Int GetCellCoordinates(Vector3 pointerPosition)
+        {
+            var worldPoint = Camera.main.ScreenToWorldPoint(pointerPosition);
+
+            return _renderField.WorldToCell(worldPoint);
+        }
+    
+        private void GenerateEmptyField()
+        {
+            _cells = new Cell[_width, _height];
+            for (var i = 0; i < _cells.GetLength(0); i++)
             {
-                _cells[i, j] = new Cell();
+                for (var j = 0; j < _cells.GetLength(1); j++)
+                {
+                    _cells[i, j] = new Cell();
+                }
             }
         }
     }
