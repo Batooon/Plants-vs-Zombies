1cb8a0e297938020711decc0de83e80e0b9eeb6a
Anton <rozum.ant@gmail.com>
2021-01-15 01:58:12 +0200

    IsEmpty property set accessor added

--- Assets/Scripts/Logic/Cell.cs
+++ Assets/Scripts/Logic/Cell.cs
@@ -1,5 +1,11 @@
 public class Cell
 {
     private bool _isEmpty = true;
-    public bool IsEmpty => _isEmpty;
+
+    public bool IsEmpty
+    {
+        get => _isEmpty;
+        set =>_isEmpty = value;
+    }
+
 }
