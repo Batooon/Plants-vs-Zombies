a4b6bf9fc98063d7f31972ff1a3f7e61a9d5952b
Anton <rozum.ant@gmail.com>
2021-01-16 01:13:44 +0200

    Added OnCollisionEnter2D method to damage zombies

--- Assets/Scripts/Logic/Bullet.cs
+++ Assets/Scripts/Logic/Bullet.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Logic
@@ -5,10 +6,20 @@ namespace Logic
     public class Bullet : MonoBehaviour
     {
         [SerializeField] private float _speed;
+        [SerializeField] private int _damage;
 
         private void Update()
         {
             transform.position += Vector3.right * _speed * Time.deltaTime;
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.TryGetComponent(out Zombie zombie))
+            {
+                zombie.GetDamage(_damage);
+                Destroy(gameObject);
+            }
         }
     }
 }
