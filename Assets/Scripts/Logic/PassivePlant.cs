fbf4b7a9cd37db21f904ed4ed4d829757a3de9b0
Anton <rozum.ant@gmail.com>
2021-01-17 00:46:31 +0200

    Spawning PlantPresenter prefab

--- Assets/Scripts/Logic/PassivePlant.cs
+++ Assets/Scripts/Logic/PassivePlant.cs
@@ -5,8 +5,9 @@ namespace Logic
     public class PassivePlant : Plant
     {
         [SerializeField] private float _delayAction;
-        [SerializeField] private GameObject _objectToInstantiate;
+        [SerializeField] private SunPresenter _objectToInstantiate;
         [SerializeField] private Transform _spawnPoint;
+        [SerializeField] private int _sunAmount;
 
         private void Start()
         {
@@ -15,7 +16,9 @@ namespace Logic
 
         private void Spawn()
         {
-            
+            SunPresenter spawnedSun = Instantiate(_objectToInstantiate.gameObject, _spawnPoint.position, Quaternion.identity)
+                .GetComponent<SunPresenter>();
+            spawnedSun.Init(_sunAmount, _playerData);
         }
     }
 }
