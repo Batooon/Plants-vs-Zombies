2d6106df18eb9b278f6b5435c94548a6e7b84353
Anton <rozum.ant@gmail.com>
2021-01-17 00:42:51 +0200

    Player presenter added

---
+++ Assets/Scripts/PlayerPresenter.cs
@@ -0,0 +1,32 @@
+using Data;
+using TMPro;
+using UnityEngine;
+
+public class PlayerPresenter : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _sunsAmount;
+    [SerializeField] private string _sunsAmountTemplate;
+
+    private PlayerData _playerData;
+    
+    public void Init(PlayerData playerData)
+    {
+        _playerData = playerData;
+    }
+
+    private void OnEnable()
+    {
+        _playerData.SunsAmountChanged += OnSunsAmountChanged;
+        OnSunsAmountChanged(_playerData.SunsAmount);
+    }
+
+    private void OnDisable()
+    {
+        _playerData.SunsAmountChanged -= OnSunsAmountChanged;
+    }
+
+    private void OnSunsAmountChanged(int newSunsAmount)
+    {
+        _sunsAmount.text = string.Format(_sunsAmountTemplate, newSunsAmount);
+    }
+}
