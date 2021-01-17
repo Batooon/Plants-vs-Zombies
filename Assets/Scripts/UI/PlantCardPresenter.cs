6cd14726ff678e5e3ca8a7e0e8ecc76485e1bee0
Anton <rozum.ant@gmail.com>
2021-01-17 23:51:53 +0200

    restores alpha of canvas group over time after plant is buyed

--- Assets/Scripts/UI/PlantCardPresenter.cs
+++ Assets/Scripts/UI/PlantCardPresenter.cs
@@ -1,20 +1,54 @@
-﻿using Data;
+﻿using System.Collections;
+using Data;
+using Logic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace UI
 {
+    [RequireComponent(typeof(CanvasGroup))]
     public class PlantCardPresenter : MonoBehaviour
     {
         [SerializeField] private Image _plantImage;
         [SerializeField] private string _costTemplate;
         [SerializeField] private TextMeshProUGUI _costText;
+        [SerializeField] private float _restoreStartingAlpha;
+        [SerializeField] private float _restoreMaxAlpha;
+        private PlantCard _plantCard;
+        private CanvasGroup _canvasGroup;
+        private PlantShopData _plantShopData;
 
-        public void Init(PlantShopData plantShopData)
+        public void Init(PlantShopData plantShopData, PlantCard plantCard)
         {
+            _plantCard = plantCard;
+            _plantShopData = plantShopData;
+            _canvasGroup = GetComponent<CanvasGroup>();
             _plantImage.sprite = plantShopData.Icon;
             _costText.text = string.Format(_costTemplate, plantShopData.Cost);
+            _plantCard.PlantBuyed += StartRestoring;
+        }
+
+        private void OnDisable()
+        {
+            _plantCard.PlantBuyed -= StartRestoring;
+        }
+
+        private void StartRestoring()
+        {
+            StartCoroutine(RestoreCard());
+        }
+
+        private IEnumerator RestoreCard()
+        {
+            for (float i = 0; i < _plantShopData.RestoreTime; i+=Time.deltaTime)
+            {
+                float normalizedTime = i / _plantShopData.RestoreTime;
+                _canvasGroup.alpha = Mathf.Lerp(_restoreStartingAlpha, _restoreMaxAlpha, normalizedTime);
+                yield return null;
+            }
+            _plantCard.OnCardRestored();
+            _canvasGroup.alpha = _restoreMaxAlpha;
         }
     }
 }
