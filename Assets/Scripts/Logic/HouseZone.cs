eaf8a9d39dcd4ec389fdc2bd432117ad5f4b0d7f
Anton <rozum.ant@gmail.com>
2021-02-25 00:18:35 +0200

    Removed unnecessary initialization of trigger collider

--- Assets/Scripts/Logic/HouseZone.cs
+++ Assets/Scripts/Logic/HouseZone.cs
@@ -8,14 +8,9 @@ namespace Logic
     {
         public event Action ZombieEntersHouse;
 
-        public void Init()
-        {
-            GetComponent<Collider2D>().isTrigger = true;
-        }
-
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if (other.TryGetComponent(out Zombie zombie))
+            if (other.TryGetComponent(out Zombie _))
             {
                 ZombieEntersHouse?.Invoke();
             }
