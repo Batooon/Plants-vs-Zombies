fc882aa5375303f90ef848e2896b774edffb5a71
Anton <rozum.ant@gmail.com>
2021-03-04 20:31:03 +0200

    Refactor

--- Assets/Scripts/Logic/Zombies/Zombie.cs
+++ Assets/Scripts/Logic/Zombies/Zombie.cs
@@ -4,26 +4,20 @@ using UnityEngine;
 namespace PvZ.Logic.Zombies
 {
     [RequireComponent(typeof(ZombieStateMachine))]
-    public class Zombie : MonoBehaviour
+    public class Zombie : MonoBehaviour, IDamageable
     {
         [SerializeField] private float _delayAttack;
         [SerializeField] private int _damage;
         [SerializeField] private int _health;
         [SerializeField] private float _speed;
+        [SerializeField] private Animator _animator;
 
         private ZombieStateMachine _stateMachine;
 
         public void Init()
         {
             _stateMachine = GetComponent<ZombieStateMachine>();
-            _stateMachine.Init();
-        }
-
-        public void GetDamage(int amount)
-        {
-            _health -= amount;
-            if (_health <= 0)
-                Die();
+            _stateMachine.Init(_animator);
         }
 
         private void Die()
@@ -34,6 +28,13 @@ namespace PvZ.Logic.Zombies
         public static bool CollidedWithPlant(out Plant plant, Collision2D other)
         {
             return other.gameObject.TryGetComponent(out plant);
+        }
+
+        public void ApplyDamage(int damage)
+        {
+            _health -= damage;
+            if(_health<=0)
+                Die();
         }
     }
 }
